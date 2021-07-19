be6840966a7dd16e3e7e4b4d5567c1e93d9e2798
Hoyoung Son <xzxking17@gmail.com>
2021-07-19 17:49:52 +0900

    feat: :sparkles: pages/index.tsx 수정 (좀 더 편한 import를 위함)

--- src/pages/EnterPage.tsx
+++ src/pages/EnterPage.tsx
@@ -1,3 +1,3 @@
-// 모집 페이지
+// 진입 페이지
 const EnterPage = () => <div>EnterPage</div>;
 export default EnterPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
 // pages : index
-import EnterPage from "./EnterPage";
-export { EnterPage };
+import EnterPage from './EnterPage';
+import HostRegistrationPage from './HostRegistrationPage';
+import LoginPage from './LoginPage';
+import MainPage from './MainPage';
+import MyInfoPage from './MyInfoPage';
+import RecruitmentDetailPage from './RecruitmentDetailPage';
+import RecruitmentsPage from './RecruitmentsPage';
+import RegisterInterestPage from './RegisterInterestPage';
+import RoomBookingPage from './RoomBookingPage';
+import RoomDetailPage from './RoomDetailPage';
+
+export {
+  EnterPage,
+  HostRegistrationPage,
+  LoginPage,
+  MainPage,
+  MyInfoPage,
+  RecruitmentDetailPage,
+  RecruitmentsPage,
+  RegisterInterestPage,
+  RoomBookingPage,
+  RoomDetailPage,
+};
+
