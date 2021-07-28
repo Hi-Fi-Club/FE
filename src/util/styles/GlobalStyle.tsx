c3948e690ea3c2d8089867cb31a2321b9480e81a
Hoyoung Son <xzxking17@gmail.com>
2021-07-28 22:15:57 +0900

    [FE-17] feat: reset css 적용 (styled-reset 라이브러리 적용)

--- src/util/styles/GlobalStyle.tsx
+++ src/util/styles/GlobalStyle.tsx
@@ -1,11 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 // --- Styled Components ---
 const GlobalStyle = createGlobalStyle`
-  #root {
-    width: 100%;
-    min-height: 100vh;
-    margin: 0 auto;
+  ${reset};
+	* {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: -apple-system, Helvetica, Arial, "hiragino kaku gothic pro", meiryo, "Microsoft YaHei", "ms pgothic", "Apple SD Gothic Neo", "Nanum Gothic", "Malgun Gothic", sans-serif;
+    font-size: 18px;
+    line-height: 21px;
+    color: #212529;
+  }
+  a, a:active, a:hover, a:visited{
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  ul, ol {
+    list-style: none;
+  }
+  input, button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    cursor: pointer;
   }
 `;
 
