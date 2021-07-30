2f69e9bc08e8fc1d3d212db68726dc870c9438ec
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-31 01:47:51 +0900

    [FE-25] feat : Nunito 폰트 글로벌 적용

---
+++ src/fonts/font.js
@@ -0,0 +1,17 @@
+import Nunito_Black from "./Nunito_Black.woff";
+import Nunito_ExtraBold from "./Nunito_ExtraBold.woff";
+import Nunito_Bold from "./Nunito_Bold.woff";
+import Nunito_SemiBold from "./Nunito_SemiBold.woff";
+import Nunito_Regular from "./Nunito_Regular.woff";
+import Nunito_Light from "./Nunito_Light.woff";
+import Nunito_ExtraLight from "./Nunito_ExtraLight.woff";
+
+export {
+  Nunito_Black,
+  Nunito_ExtraBold,
+  Nunito_Bold,
+  Nunito_SemiBold,
+  Nunito_Regular,
+  Nunito_Light,
+  Nunito_ExtraLight,
+};
--- src/util/styles/GlobalStyle.tsx
+++ src/util/styles/GlobalStyle.tsx
@@ -1,14 +1,69 @@
-import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
+import { createGlobalStyle } from "styled-components";
+import {
+  Nunito_Black,
+  Nunito_ExtraBold,
+  Nunito_Bold,
+  Nunito_SemiBold,
+  Nunito_Regular,
+  Nunito_Light,
+  Nunito_ExtraLight,
+} from "./../../fonts/font.js";
+
+import reset from "styled-reset";
 
 // --- Styled Components ---
 const GlobalStyle = createGlobalStyle`
   ${reset};
+
+
+  @font-face {
+    font-family: 'Nunito_Black';	
+    src: local('Nunito_Black'),    
+    url(${Nunito_Black})format('woff');
+  }
+
+  @font-face {
+    font-family: 'Nunito_ExtraBold';	
+    src: local('Nunito_ExtraBold'),    
+    url(${Nunito_ExtraBold})format('woff');
+  }
+
+  @font-face {
+    font-family: 'Nunito_Bold';	
+    src: local('Nunito_Bold'),    
+    url(${Nunito_Bold})format('woff');
+  }
+
+  @font-face {
+    font-family: 'Nunito_SemiBold';	
+    src: local('Nunito_SemiBold'),    
+    url(${Nunito_SemiBold})format('woff');
+  }
+
+  @font-face {
+    font-family: 'Nunito_Regular';	
+    src: local('Nunito_Regular'),    
+    url(${Nunito_Regular})format('woff');
+  }
+
+  @font-face {
+    font-family: 'Nunito_Light';	
+    src: local('Nunito_Light'),    
+    url(${Nunito_Light})format('woff');
+  }
+
+  @font-face {
+    font-family: 'Nunito_ExtraLight';	
+    src: local('Nunito_ExtraLight'),    
+    url(${Nunito_ExtraLight})format('woff');
+  }
+  
 	* {
     box-sizing: border-box;
+    font-family:'Nunito_Regular';
   }
+  
   body {
-    font-family: -apple-system, Helvetica, Arial, "hiragino kaku gothic pro", meiryo, "Microsoft YaHei", "ms pgothic", "Apple SD Gothic Neo", "Nanum Gothic", "Malgun Gothic", sans-serif;
     font-size: 18px;
     line-height: 21px;
     color: #212529;
