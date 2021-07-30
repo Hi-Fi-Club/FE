050127e3a47470014ce07366c0c8c50df126056b
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-31 01:49:40 +0900

    [FE-3] WIP : 엔트리 페이지 마크업 작업중

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,43 @@
+import styled from "styled-components";
+import LoginButton from "components/Common/Buttons/BlueButton";
+
 const Header = () => (
   // 좌우 끝
-  <div>
-    <span>Logo</span>
-    <span>유저정보/로그인</span>
-  </div>
+  <HeaderLayout>
+    <HeaderContentsWrapper>
+      <Logo />
+      <ButtonContainer>
+        <LoginButton width={79} height={32} font={"Nunito_Black"} fontSize={18}>
+          Sign In
+        </LoginButton>
+      </ButtonContainer>
+    </HeaderContentsWrapper>
+  </HeaderLayout>
 );
 
 export default Header;
+
+const HeaderLayout = styled.div`
+  height: 64px;
+  box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
+`;
+
+const HeaderContentsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+`;
+
+const Logo = styled.div`
+  height: 60%;
+  width: 140px;
+  margin: 0 100px;
+  background-image: url("/logoType_1.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
+
+const ButtonContainer = styled.div`
+  margin: 0 100px;
+`;
