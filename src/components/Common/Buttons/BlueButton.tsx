de0f01c9c0aab53f39cdf1677a1bd98aa696575f
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-07-31 01:51:19 +0900

    [FE-26] feat : 공통 컴포넌트 파란 버튼 제작

---
+++ src/components/Common/Buttons/BlueButton.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { TChildren } from "util/types";
+
+interface button {
+  width: number;
+  height: number;
+  font: string;
+  fontSize: number;
+  children: TChildren;
+}
+
+const BlueButton = ({ width, height, fontSize, font, children }: button) => {
+  return (
+    <ButtonLayout width={width} height={height} fontSize={fontSize} font={font}>
+      {children}
+    </ButtonLayout>
+  );
+};
+
+export default BlueButton;
+
+const ButtonLayout = styled.button<button>`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  font-family: ${({ font }) => font};
+  font-size: ${({ fontSize }) => fontSize}px;
+  color: ${({ theme }) => theme.grayScaleColors.offWhite};
+  background-color: ${({ theme }) => theme.colors.secondary};
+  font-family: "Nunito_Black";
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 25px;
+`;
