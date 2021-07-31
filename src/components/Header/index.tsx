3ba5905f25e58cd43885a5408ce3245bbd322fa4
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-08-01 02:38:20 +0900

    [FE-28] feat : 헤더 컴포넌트 스크롤에 따른 투명도 적용

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,43 @@
+import { useState } from "react";
 import styled from "styled-components";
 import LoginButton from "components/Common/Buttons/BlueButton";
+import { TPage } from "util/types";
 
-const Header = () => (
-  // 좌우 끝
-  <HeaderLayout>
-    <HeaderContentsWrapper>
-      <Logo />
-      <ButtonContainer>
-        <LoginButton width={79} height={32} font={"Nunito_Black"} fontSize={18}>
-          Sign In
-        </LoginButton>
-      </ButtonContainer>
-    </HeaderContentsWrapper>
-  </HeaderLayout>
-);
+interface Props {
+  children?: HTMLDivElement;
+  page: TPage;
+}
+
+const Header = ({ page }: Props) => {
+  const [headerState, setHeaderState] = useState(true);
+
+  window.addEventListener("scroll", () => {
+    window.scrollY === 0 && page === "enter" ? setHeaderState(true) : setHeaderState(false);
+  });
+
+  return (
+    <HeaderLayout headerState={headerState}>
+      <HeaderContentsWrapper>
+        <Logo />
+        <ButtonContainer>
+          <LoginButton width={79} height={32} font={"Nunito_Black"} fontSize={18}>
+            Sign In
+          </LoginButton>
+        </ButtonContainer>
+      </HeaderContentsWrapper>
+    </HeaderLayout>
+  );
+};
 
 export default Header;
 
-const HeaderLayout = styled.div`
+const HeaderLayout = styled.div<{ headerState: Boolean }>`
+  width: 100%;
   height: 64px;
-  box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
+  position: fixed;
+  box-shadow: ${({ headerState }) => (headerState ? "none" : "0.3em 0.3em 1em rgba(0, 0, 0, 0.3)")};
+  background-color: ${({ headerState }) => (headerState ? "transparent" : "#ffffff")};
+  transition: background-color 0.4s;
 `;
 
 const HeaderContentsWrapper = styled.div`
@@ -27,17 +45,15 @@ const HeaderContentsWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   height: 100%;
+  margin: 0 200px;
 `;
 
 const Logo = styled.div`
   height: 60%;
   width: 140px;
-  margin: 0 100px;
   background-image: url("/logoType_1.png");
   background-repeat: no-repeat;
   background-size: contain;
 `;
 
-const ButtonContainer = styled.div`
-  margin: 0 100px;
-`;
+const ButtonContainer = styled.div``;
