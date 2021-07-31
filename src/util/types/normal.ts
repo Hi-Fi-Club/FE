f7c2b4d0cbf12aa2a4b95e18cf5bbf3187ff7e72
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-08-01 02:37:00 +0900

    chore: 폰트, 페이지 타입 추가

--- src/util/types/normal.ts
+++ src/util/types/normal.ts
@@ -7,4 +7,15 @@ type TChildren =
   | NonNullable<React.ReactNode>
   | {};
 
-export type { TChildren };
+type TFont =
+  | "Nunito_Black"
+  | "Nunito_ExtraBold"
+  | "Nunito_Bold"
+  | "Nunito_SemiBold"
+  | "Nunito_Regular"
+  | "Nunito_Light"
+  | "Nunito_ExtraLight";
+
+type TPage = "enter" | "login";
+
+export type { TChildren, TFont, TPage };
