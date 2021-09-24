6d3c751564654bf8688adbc44b3a7d34f1c1064a
Namju <skawn11114@gmail.com>
2021-09-24 17:12:35 +0900

    [#37] Refactor : 컴포넌트 기능별 모듈화 - 입력 - 결과 - 선택결과

--- src/components/User/RegisterLocation/index.tsx
+++ src/components/User/RegisterLocation/index.tsx
@@ -1,2 +1,19 @@
 import RegisterLocation from "./RegisterLocation";
+type Address = {
+  [key: string]: string;
+};
+export interface LocationResult {
+  address: Address;
+  address_name: string;
+  address_type: string;
+  road_address: null;
+  x: string;
+  y: string;
+}
+
+export const getLocationName = (location: string) => {
+  const parseLocation = location.split(" ").reverse();
+  return { cityName: parseLocation[1], dongName: parseLocation[0] };
+};
+
 export default RegisterLocation;
